248f3e39352038488c13f142ebc0320989b8cf0e
Amjadyabroudi128 <61939508+Amjadyabroudi128@users.noreply.github.com>
2023-09-27 01:48:51 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class _DemoState extends State<Demo> {
                       children: [
                         Row(
                           children: [
-                          Text('on off you choose', style: TextStyle(fontWeight: FontWeight.bold)),
+                            Text('on off you choose', style: TextStyle(fontWeight: FontWeight.bold)),
                             Spacer(),
                             CupertinoSwitch(
                               value: isSwitched,
@@ -83,8 +83,9 @@ class _DemoState extends State<Demo> {
                         ),
                         Row(
                           children: [
-                            Text('DATE', style: TextStyle(fontWeight: FontWeight.bold),),
-                            
+                            Text('Date'),
+                           Spacer(),
+                           DatePickerExample(),
                           ],
                         )
                       ],
@@ -98,20 +99,20 @@ class _DemoState extends State<Demo> {
               Text('second section', style: TextStyle(color: Colors.grey),),
               SizedBox(height: 10,),
               Card(
-                child: Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('hello World', style: TextStyle(fontWeight: FontWeight.bold),),
-                      Divider(color: Colors.black, thickness: 1,),
-                      Text('hello World', style: TextStyle(fontWeight: FontWeight.bold),),
-                      Divider(color: Colors.black, thickness: 1,),
-                      Text('hello World', style: TextStyle(fontWeight: FontWeight.bold),),
-                      Divider(color: Colors.black, thickness: 1,),
-                      Text('hello World', style: TextStyle(fontWeight: FontWeight.bold),),
-                    ],
-                  ),
-                )
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('hello World', style: TextStyle(fontWeight: FontWeight.bold),),
+                        Divider(color: Colors.black, thickness: 1,),
+                        Text('hello World', style: TextStyle(fontWeight: FontWeight.bold),),
+                        Divider(color: Colors.black, thickness: 1,),
+                        Text('hello World', style: TextStyle(fontWeight: FontWeight.bold),),
+                        Divider(color: Colors.black, thickness: 1,),
+                        Text('hello World', style: TextStyle(fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                  )
               )
             ],
           ),
@@ -122,3 +123,63 @@ class _DemoState extends State<Demo> {
   }
 }
 
+class DatePickerExample extends StatefulWidget {
+  const DatePickerExample({Key? key}) : super(key: key);
+
+  @override
+  State<DatePickerExample> createState() => _DatePickerExampleState();
+}
+
+class _DatePickerExampleState extends State<DatePickerExample> {
+  DateTime date = DateTime(2016, 10, 26);
+  void _showDialog(Widget child) {
+    showCupertinoModalPopup<void>(
+      context: context,
+      builder: (BuildContext context) => Container(
+        height: 216,
+        padding: const EdgeInsets.only(top: 6.0),
+        // The Bottom margin is provided to align the popup above the system
+        // navigation bar.
+        margin: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        // Provide a background color for the popup.
+        color: CupertinoColors.systemBackground.resolveFrom(context),
+        // Use a SafeArea widget to avoid system overlaps.
+        child: SafeArea(
+          top: false,
+          child: child,
+        ),
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoButton(
+      // Display a CupertinoDatePicker in date picker mode.
+      onPressed: () => _showDialog(
+        CupertinoDatePicker(
+          initialDateTime: date,
+          mode: CupertinoDatePickerMode.date,
+          use24hFormat: true,
+          // This shows day of week alongside day of month
+          showDayOfWeek: true,
+          // This is called when the user changes the date.
+          onDateTimeChanged: (DateTime newDate) {
+            setState(() => date = newDate);
+          },
+        ),
+      ),
+      // In this example, the date is formatted manually. You can
+      // use the intl package to format the value based on the
+      // user's locale settings.
+      child: Text(
+        '${date.month}-${date.day}-${date.year}',
+        style: const TextStyle(
+          fontSize: 22.0,
+        ),
+      ),
+    );
+
+  }
+}
